f233a717e2d0ee88af0a2c8f5c5af958e95bc2ef
stcakova <s_tzakova@abv.bg>
2014-03-15 23:41:16 +0200

    Create sample_tests.py

---
+++ sample_tests.py
@@ -0,0 +1,32 @@
+import unittest
+
+import solution
+
+
+class TestNarcissistic(unittest.TestCase):
+
+    def test_is_narcissistic(self):
+        self.assertFalse(solution.is_narcissistic('10'))
+
+        self.assertTrue(solution.is_narcissistic('223', 4))
+
+        self.assertTrue(solution.is_narcissistic('0'))
+
+        self.assertTrue(solution.is_narcissistic('1'))
+
+        self.assertTrue(solution.is_narcissistic('54748'))
+
+        self.assertTrue(solution.is_narcissistic('5A07C', 16))
+
+        self.assertTrue(solution.is_narcissistic('13579', 16))
+
+        self.assertTrue(solution.is_narcissistic('124030', 5))
+
+        self.assertTrue(solution.is_narcissistic('570', 9))
+
+        self.assertFalse(solution.is_narcissistic('103'))
+
+        self.assertTrue(solution.is_narcissistic('122', 3))
+
+if __name__ == '__main__':
+    unittest.main()
